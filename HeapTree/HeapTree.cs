ecdf91a7df4130f1d67bdb4935ddfe1533d865fe
Iggy Glassman <Iggy.Glassman@GMR.local>
2019-11-09 10:36:39 -0800

    Inital Commit

---
+++ HeapTree/HeapTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeapTree
+{
+    class HeapTree<T> where T : IComparable
+    {
+
+        private T[] tree;
+
+        private void expand(int amount)
+        {
+            T[] newTree = new T[tree.Length + amount];
+
+            for (int i = 0; i < tree.Length; i++)
+            {
+                newTree[i] = tree[i];
+            }
+
+            tree = newTree;
+        }
+    }
+}
---
+++ HeapTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HeapTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
